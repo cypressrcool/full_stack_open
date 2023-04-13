d4757de01e68c823fe9953ffac7c6ead73bf0ba3
cypressrcool <91033018+cypressrcool@users.noreply.github.com>
2023-04-13 20:53:02 +0530

    exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
@@ -11,7 +11,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  const [part1, part2, part3] = props.parts
+  const [part1, part2, part3] = props.course.parts
   return (
     <div>
       <Part part = {part1} />
@@ -23,7 +23,7 @@ const Content = (props) => {
 
 const Total = (props) => {
   let total = 0
-  const [part1, part2, part3] = props.parts
+  const [part1, part2, part3] = props.course.parts
   total = part1.exercises + part2.exercises + part3.exercises
 
   return (
@@ -33,27 +33,29 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts = {parts} />
-      <Total parts = {parts}/>
+      <Content course = {course} />
+      <Total course = {course}/>
     </div>
   )
 }
