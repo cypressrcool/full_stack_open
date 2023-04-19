c2bdb370779845872758999f0ea62b3abb7e5a06
cypressrcool <91033018+cypressrcool@users.noreply.github.com>
2023-04-19 15:49:24 +0530

    phonebook exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number: '040-1234567'}
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
 
   const handleSubmitForm = (event) => {
@@ -16,19 +17,23 @@ const App = () => {
     }
     else {
       const newPersonsObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(newPersonsObject))
     }
 
     
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
-
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
@@ -37,12 +42,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       
-      {persons.map(person=><div key={person.name}>{person.name}</div>)}
+      {persons.map(person=><div key={person.name}>{person.name} {person.number}</div>)}
       
     </div>
   )
