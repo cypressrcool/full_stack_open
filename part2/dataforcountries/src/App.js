0b2c0059f6759937f8eec228e642def9dabf1c7d
cypressrcool <91033018+cypressrcool@users.noreply.github.com>
2023-04-23 02:27:06 +0530

    dataforcountries exercise 2.20

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,9 +1,27 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
 const SingleCountry = ({country}) => {
+  const api_key = process.env.REACT_APP_API_KEY
+  const [lat, lng] = country.capitalInfo.latlng
+  const [temp, setTemp] = useState(null)
+  const [wind, setWind] = useState(null)
+  const [icon, setIcon] = useState(null)
 
+  useEffect(()=> {
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api_key}&units=metric`)
+    .then(response => {
+        setTemp(response.data.main.temp)
+        setWind(response.data.wind.speed)
+        setIcon(response.data.weather[0].icon)
+    })
+  }, [])
+  
+  if (!icon){
+    return null
+  }
   return (
-    <div className="singleCountrybox">
+    <div>
       <h1>{country.name.common}</h1>
       <div className="information">
         <div>capital {country.capital}</div>
@@ -14,6 +32,10 @@ const SingleCountry = ({country}) => {
         {Object.values(country.languages).map(l => <li key={l}>{l}</li>)}
       </ul>
       <img src={country.flags.png} alt={country.flags.alt} />
+      <h2>Weather in {country.capital}</h2>
+      <div>temperature {temp} Celcius</div>
+      <div><img src= {`https://openweathermap.org/img/wn/${icon}@2x.png`}/></div>
+      <div>wind {wind} m/s</div>
     </div>
   )
 }
@@ -65,7 +87,7 @@ const App = () => {
   useEffect(()=>{
     if (value){
       axios
-      .get('https://restcountries.com/v3.1/all?fields=name,flags,languages,area,capital')
+      .get('https://restcountries.com/v3.1/all?fields=name,flags,languages,area,capital,capitalInfo')
       .then(response => {
         const allCountries = response.data
         const filteredCountries = allCountries.filter(c => c.name.common.toLowerCase().includes(value))
